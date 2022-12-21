91e2a468a37bcc503237a9b25f5e76b25dfbdb9c
Dhika090 <nyes6953@gmail.com>
2022-12-21 15:13:10 +0700

    fix text change

--- pages/cart/checkout.tsx
+++ pages/cart/checkout.tsx
@@ -22,19 +22,19 @@ const CheckoutPage = () => {
       <section className="cart">
         <div className="container">
           <div className="cart__intro">
-            <h3 className="cart__title">Shipping and Payment</h3>
+            <h3 className="cart__title">Pengiriman dan Pembayaran</h3>
             <CheckoutStatus step="checkout" />
           </div>
 
           <div className="checkout-content">
             <div className="checkout__col-6">
               <div className="checkout__btns">
-                <button className="btn btn--rounded btn--yellow">Log in</button>
-                <button className="btn btn--rounded btn--border">Sign up</button>
+                <button className="btn btn--rounded btn--yellow">Masuk</button>
+                <button className="btn btn--rounded btn--border">Daftar</button>
               </div>
 
               <div className="block">
-                <h3 className="block__title">Shipping information</h3>
+                <h3 className="block__title">Informasi Pengiriman</h3>
                 <form className="form">
                   <div className="form__input-row form__input-row--two">
                     <div className="form__col">
@@ -42,33 +42,33 @@ const CheckoutPage = () => {
                     </div>
 
                     <div className="form__col">
-                      <input className="form__input form__input--sm" type="text" placeholder="Address" />
+                      <input className="form__input form__input--sm" type="text" placeholder="Alamat" />
                     </div>
                   </div>
                   
                   <div className="form__input-row form__input-row--two">
                     <div className="form__col">
-                      <input className="form__input form__input--sm" type="text" placeholder="First name" />
+                      <input className="form__input form__input--sm" type="text" placeholder="Nama Depan" />
                     </div>
 
                     <div className="form__col">
-                      <input className="form__input form__input--sm" type="text" placeholder="City" />
+                      <input className="form__input form__input--sm" type="text" placeholder="Kota" />
                     </div>
                   </div>
                   
                   <div className="form__input-row form__input-row--two">
                     <div className="form__col">
-                      <input className="form__input form__input--sm" type="text" placeholder="Last name" />
+                      <input className="form__input form__input--sm" type="text" placeholder="Nama Belakang" />
                     </div>
 
                     <div className="form__col">
-                      <input className="form__input form__input--sm" type="text" placeholder="Postal code / ZIP" />
+                      <input className="form__input form__input--sm" type="text" placeholder="Kode Pos" />
                     </div>
                   </div>
 
                   <div className="form__input-row form__input-row--two">
                     <div className="form__col">
-                      <input className="form__input form__input--sm" type="text" placeholder="Phone number" />
+                      <input className="form__input form__input--sm" type="text" placeholder="Nomor Telepon" />
                     </div>
 
                     <div className="form__col">
@@ -110,23 +110,23 @@ const CheckoutPage = () => {
               </div>
               
               <div className="block">
-                <h3 className="block__title">Jasa Pengantaran</h3>
+                <h3 className="block__title">Jasa Pengiriman</h3>
                 <ul className="round-options round-options--two">
                   <li className="round-item round-item--bg">
                     <img src="/images/logos/inpost.svg" alt="Paypal" />
-                    <p>Rp20.00</p>
+                    <p>Rp20.000</p>
                   </li>
                   <li className="round-item round-item--bg">
                     <img src="/images/logos/dpd.svg" alt="Paypal" />
-                    <p>Rp12.00</p>
+                    <p>Rp12.000</p>
                   </li>
                   <li className="round-item round-item--bg">
                     <img src="/images/logos/dhl.svg" alt="Paypal" />
-                    <p>Rp15.00</p>
+                    <p>Rp15.000</p>
                   </li>
                   <li className="round-item round-item--bg">
                     <img src="/images/logos/maestro.png" alt="Paypal" />
-                    <p>Rp10.00</p>
+                    <p>Rp10.000</p>
                   </li>
                 </ul>
               </div>
