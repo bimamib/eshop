c361868d5a87d6247fd8124d327869d19dca8d19
dhika090 <nyes6953@gmail.com>
2022-12-21 14:58:23 +0700

    change swiperslide content

--- components/page-intro/index.tsx
+++ components/page-intro/index.tsx
@@ -1,15 +1,17 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, {EffectFade, Navigation} from 'swiper';
 
+import 'swiper/css';
+
 SwiperCore.use([EffectFade, Navigation]);
 
 const PageIntro = () => {
 
   return (
     <section className="page-intro">  
-      <Swiper navigation effect="fade" className="swiper-wrapper">
-        {/* <SwiperSlide>
-          <div className="page-intro__slide" style={{ backgroundImage: "url('/images/slide-1.jpg')" }}>
+      <Swiper navigation effect="coverflow" className="swiper-wrapper">
+        <SwiperSlide>
+          <div className="page-intro__slide" style={{ backgroundImage: "url('/images/Makramee/banner1.jpg')" }}>
             <div className="container">
               <div className="page-intro__slide__content">
                 <h2>Sale of the summer collection</h2>
@@ -17,10 +19,10 @@ const PageIntro = () => {
               </div>
             </div>
           </div>
-        </SwiperSlide> */}
+        </SwiperSlide>
 
         <SwiperSlide>
-          <div className="page-intro__slide" style={{ backgroundImage: "url('/images/Makramee/banner1.jpg')" }}>
+          <div className="page-intro__slide" style={{ backgroundImage: "url('/images/Makramee/1.jpg')" }}>
             <div className="container">
               <div className="page-intro__slide__content">
                 <h2>Menjual Berbagai Jenis Macrame</h2>
