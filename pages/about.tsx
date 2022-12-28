8b65213198f708fe468d7e1f3bcfc763d59a1623
dhika090 <nyes6953@gmail.com>
2022-12-28 23:01:45 +0700

    added teks account

---
+++ pages/about.tsx
@@ -0,0 +1,16 @@
+import Layout from '../layouts/main';
+import Footer from '../components/footer';
+import  Image  from 'next/image';
+
+const About = ()=> {
+  return (
+    <Layout>
+     
+
+      <Footer />
+    </Layout>
+      
+  )
+}
+
+export default About
