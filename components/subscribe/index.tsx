3cdba57bdb970c2dbf0819412f84da4c14c32ea1
dhika090 <nyes6953@gmail.com>
2023-01-03 22:22:03 +0700

    fixing img subscribe

--- components/subscribe/index.tsx
+++ components/subscribe/index.tsx
@@ -1,8 +1,12 @@
+import Image from "next/image";
+
 const Subscribe = () => {
     return (
       <section className="subscribe">
         <div className="container">
-          <div style={{backgroundImage: 'url(/images/Makramee/Banner-Subscribe2.jpg)'}} className="subscribe__content">
+          <div className="subscribe__content">
+            
+          <Image src={'/images/Makramee/banner11.jpg'} alt={''} layout="fill"/>
             <h4>Segera Berlangganan untuk mendapatkan Hadiah dan Promo menarik lainnya pada setiap Minggu Ceria</h4>
             <form className="subscribe__form">
               <input type="email" placeholder="Email address" />
