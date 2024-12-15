a0dacb4afb96b5110b2f6802b4974c1ab0425e60
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-12-15 15:20:21 +0700

    Edit text justify

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
-import Logo from '../../assets/icons/logo';
+import Link from "next/link";
+import Logo from "../../assets/icons/logo";
 
 const Footer = () => {
   return (
@@ -7,44 +7,69 @@ const Footer = () => {
       <div className="container">
         <div className="site-footer__top">
           <div className="site-footer__description">
-            <h6><Logo /> <span>Me</span>-Shop</h6>
-            <p>Website ini menyediakan berbagai jenis Macrame dan Dream Catcher yang dapat dibeli oleh pengunjung.
-               Bisa juga menyertakan kategori berdasarkan jenis Macrame dan Dream Catcher, seperti Macrame ataupun 
-               Dream Catcher untuk dekorasi rumah, Macrame, Dream Catcher untuk aksesoris, dll.</p>
+            <h6>
+              <Logo /> <span>Me</span>-Shop
+            </h6>
+            <p className="text-justify">
+              Website ini menyediakan berbagai jenis Macrame dan Dream Catcher
+              yang dapat dibeli oleh pengunjung. Bisa juga menyertakan kategori
+              berdasarkan jenis Macrame dan Dream Catcher, seperti Macrame
+              ataupun Dream Catcher untuk dekorasi rumah, Macrame, Dream Catcher
+              untuk aksesoris, dll.
+            </p>
           </div>
 
           <div className="site-footer__links">
             <ul>
               <li>Belanja Online</li>
-              <li><Link href="#">Order Status</Link></li>
-              <li><Link href="#">Pengantaran</Link></li>
-              <li><Link href="#">Garansi Pengembalian</Link></li>
-              <li><Link href="#">Opsi Pembayaran</Link></li>
+              <li>
+                <Link href="#">Order Status</Link>
+              </li>
+              <li>
+                <Link href="#">Pengantaran</Link>
+              </li>
+              <li>
+                <Link href="#">Garansi Pengembalian</Link>
+              </li>
+              <li>
+                <Link href="#">Opsi Pembayaran</Link>
+              </li>
             </ul>
             <ul>
               <li>Informasi</li>
-              <li><Link href="#">Temukan Toko Terdekat</Link></li>
-              <li><Link href="#">Produk Terbaru</Link></li>
-              <li><Link href="#">Daftar Langganan</Link></li>
-              <li><Link href="#">Kritik dan Saran</Link></li>
+              <li>
+                <Link href="#">Temukan Toko Terdekat</Link>
+              </li>
+              <li>
+                <Link href="#">Produk Terbaru</Link>
+              </li>
+              <li>
+                <Link href="#">Daftar Langganan</Link>
+              </li>
+              <li>
+                <Link href="#">Kritik dan Saran</Link>
+              </li>
             </ul>
             <ul>
               <li>Kontak</li>
-              <li><Link href="#">MeShop@uikit.com</Link></li>
-              <li><Link href="#">Hotline: +62 131 138 138</Link></li>
+              <li>
+                <Link href="#">MeShop@uikit.com</Link>
+              </li>
+              <li>
+                <Link href="#">Hotline: +62 131 138 138</Link>
+              </li>
             </ul>
           </div>
         </div>
       </div>
-      
+
       <div className="site-footer__bottom">
         <div className="container">
           <p>DESIGN BY KELOMPOK3.co</p>
         </div>
       </div>
     </footer>
-  )
+  );
 };
 
-
-export default Footer
+export default Footer;
